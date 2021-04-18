2e477df451994d1498fc1e920c12a009e74fb8fa
Anthony <anth19800@hotmail.com>
2021-04-18 02:58:34 -0400

    [Wip] AI

---
+++ Assets/Scripts/TestAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class TestAI : MonoBehaviour
+{
+    public Transform destination;
+    public NavMeshAgent navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void Update() {
+        navMeshAgent.SetDestination(destination.position);
+        
+    }
+
+}
